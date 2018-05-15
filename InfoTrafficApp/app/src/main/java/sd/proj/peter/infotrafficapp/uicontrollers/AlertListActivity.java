e71352e13150055b960d38a2318cecd0af516bfd
bloopeti <peter.zavaczki@gmail.com>
2018-05-15 16:44:53 +0300

    Complete UI design

---
+++ InfoTrafficApp/app/src/main/java/sd/proj/peter/infotrafficapp/uicontrollers/AlertListActivity.java
@@ -0,0 +1,15 @@
+package sd.proj.peter.infotrafficapp.uicontrollers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sd.proj.peter.infotrafficapp.R;
+
+public class AlertListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_list);
+    }
+}
